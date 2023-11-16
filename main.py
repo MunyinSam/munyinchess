bd3ad65d707d55f94b23ac23cf87f0698a15c891
MunyinSam <kelvinsam233@gmail.com>
2023-11-16 18:16:09 +0700

    Updated gameloop

--- main.py
+++ main.py
@@ -3,10 +3,25 @@ import pygame
 
 pygame.init()
 WIDTH = 1000
-HEIGHT = 900
+HEIGHT = 700
 screen = pygame.display.set_mode([WIDTH,HEIGHT])
 font = pygame.font.Font('freesansbold.ttf', 20)
 big_font = pygame.font.Font('freesansbold.ttf', 50)
 timer = pygame.time.Clock()
 fps = 60
+
 # game variables and images
+
+
+#main gameloop
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('dark grey')
+    
+    for event in pygame.event.get(): # getting all input
+        if event.type == pygame.QUIT: # prebuilt pygame
+            run = False
+        
+    pygame.display.flip()
+pygame.quit()
